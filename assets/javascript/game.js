79ab2b9ef229bd7e1a6acaf85c704e62f13fc851
Lance Martin <ldmartin7777@gmail.com>
2016-05-11 09:21:19 -0500

    fixed first letter going to end, and win counter not updating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,23 @@
 var hang = {
   words: ["captain","cannon","sails","gallows","sword","blackpearl","treasure",],
-  toBeGuessed: [],
   wrong: [],
+  wins: 0,
 
   pickWord: function() {
     return [Math.floor(Math.random()*this.words.length)];
   },
 
   printWrong: function() {
-    for (var i= 0; i<hang.wrong.length; i++) {
-      document.querySelector("#used").innerHTML = hang.wrong[i];
-    }
-  }
+    document.querySelector("#used").innerHTML = this.wrong.toString();
+  },
+
+  writeWins: function() {
+    var win= "<p>Wins: "+this.wins+"</p>";
+    document.querySelector("#wins").innerHTML = win;
+  },
 };
 
+//Sets toWin to 0. For every correct letter guessed add 1 to toWin. Once toWin = randomWord.length then the player wins
 var toWin = 0;
 
 //first variable calls the function pickWord to get a random index # from hang.words   The 2nd variable randomWord is set equal to the string value of randomIndex
@@ -36,7 +40,7 @@ function createSpaces() {
   }
 }
 
-//makes userGuess global
+//makes variable userGuess global
 document.onkeyup = function(event){
  	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
  	console.log(userGuess);
@@ -47,7 +51,9 @@ function updateSpaces() {
   for (var i=0; i<randomWord.length; i++){
    if(userGuess === randomWord[i]) {
      var fill = document.getElementsByClassName('spaces');
-     fill[i].innerHTML=userGuess;
+     fill[i+1].innerHTML=userGuess;
+     toWin++;
+     console.log(toWin);
    }
   }
 }
@@ -56,16 +62,21 @@ function updateSpaces() {
 //calls the createSpaces function to create underscores equal to the length of the word to be guessed.
 createSpaces();
 
+//Lets the user know they start with 0 wins
+hang.writeWins();
+
 //function to be ran for every user guess
 document.onkeyup = function(event) {
     userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    //if the user guess is part of the randomWord
     if(randomWord.indexOf(userGuess) !== -1 && hang.wrong.indexOf(userGuess) ===-1){
 			console.log("Letter Contained");
-      toWin++;
       console.log(toWin);
 			updateSpaces();
 		}
 
+    //if the user guess isn't part of the word
     else {
 			console.log("Letter not Contained");
 			guesses--;
@@ -73,16 +84,28 @@ document.onkeyup = function(event) {
       document.querySelector("#guess").innerHTML = guessLeft;
 			console.log(guesses);
 			hang.wrong.push(userGuess);
+      console.log(hang.wrong);
       hang.printWrong();
 			}
 
+      //if the user runs out of guesses alert them they've lost
       if( guesses === 0) {
-        alert("Sorry you ran out of guesses. Refresh this page to play again");
+        alert("Your pirate has been hung.");
+        hang.pickWord();
+        createSpaces();
+        console.log(randomWord);
+        guesses= randomWord.length*2;
+      }
+
+      //if the user guesses the word then add 1 to wins and reset the game
+      if( toWin === randomWord.length) {
+        toWin= 0;
+        hang.wins++;
+        hang.writeWins();
+        hang.pickWord();
+        createSpaces();
+        alert("You saved your pirate from the hangman, and you found some treasure while you were at it!");
       }
 };
-
-
-console.log(randomIndex);
 console.log(randomWord);
-console.log(hang.words[randomIndex]);
 console.log(guesses);
